eeef99795c3c66a056bf13995e83e00286d40781
simbyungki <dung8524@gmail.com>
2019-02-21 10:56:40 +0900

    bmi 계산 추가

--- standard_weight/js/weight.js
+++ standard_weight/js/weight.js
@@ -1,27 +1,55 @@
-const form = document.querySelector('.form')
-    , inputStature = form.querySelector('.stature')
-    , inputWeight = form.querySelector('.weight')
-    , result = document.querySelector('.result')
-    , standardTxt = result.querySelector('.standardWeight')
-    , goalTxt = result.querySelector('.goalWeight')
-    , btnReset = result.querySelector('.btnReset');
+const form = document.querySelector('.form'), 
+    inputStature = form.querySelector('.stature'), 
+    inputWeight = form.querySelector('.weight'), 
+    result = document.querySelector('.result'), 
+    resultStandardMessage = result.querySelector('.standard'),
+    bmiTxt = result.querySelector('.bmiTxt'), 
+    bmiResultTxt = result.querySelector('.bmiResultTxt'), 
+    standardTxt = result.querySelector('.standardWeight'), 
+    goalTxt = result.querySelector('.goalWeight'), 
+    btnReset = result.querySelector('.btnReset');
 
 function standardWeightCalc(stature, weight){
-    const standardWeight = (stature - 100) * 0.9;
-    const goalWeight = weight - standardWeight;
-    standardTxt.innerHTML = standardWeight;
-    goalTxt.innerHTML = goalWeight;
+    const standardWeight = stature > 150 ? (stature - 100) * 0.9 : stature - 100;
+    const bmi = Number(weight/((stature/100)*(stature/100))).toFixed(2);
+    const goalWeight = Number(weight - standardWeight).toFixed(2);
+    //bmi
+    bmiTxt.innerHTML = bmi;
+    if(bmi <= 18.5){
+        bmiResultTxt.classList.add('gray');
+        bmiResultTxt.innerHTML = '저체중';
+    }else if(bmi > 18.51 && bmi <= 24.9){
+        bmiResultTxt.classList.add('green');
+        bmiResultTxt.innerHTML = '정상';
+    }else if(bmi > 24.91 && bmi <= 29.9){
+        bmiResultTxt.classList.add('pink');
+        bmiResultTxt.innerHTML = '과체중';
+    }else if(bmi > 29.91){
+        bmiResultTxt.classList.add('red');
+        bmiResultTxt.innerHTML = '고도 비만';
+    }
+
+    //standard weight
+    if(goalWeight === 0){
+        resultStandardMessage.innerHTML = '! 표준 체중입니다.'
+    }else{
+        standardTxt.innerHTML = standardWeight;
+        goalTxt.innerHTML = goalWeight;
+    }
+
 }
 
 function handleSubmit(event){
     event.preventDefault();
     const nowStature = inputStature.value;
     const nowWeight = inputWeight.value;
-    if(nowStature === '' || nowStature < 120){
+    if(nowStature === '' || nowStature < 120 || nowStature > 210){
         alert('정확한 키를 입력해주세요.');
+        inputStature.value = '';
         inputStature.focus();
-    }else if(nowWeight === '' || nowWeight < 30){
+    }else if(nowWeight === '' || nowWeight < 30 || nowWeight > 160){
         alert('정확한 몸무게를 입력해주세요.');
+        inputWeight.value = '';
         inputWeight.focus();
     }else{
         inputStature.value = '';
@@ -34,7 +62,8 @@ function handleSubmit(event){
 
 function handleReset(){
     form.classList.remove('hide');
-    result.classList.remove('show').add('hide');
+    result.classList.remove('show');
+    bmiResultTxt.setAttribute('class', 'bmiResultTxt');
 }
 
 function init(){
